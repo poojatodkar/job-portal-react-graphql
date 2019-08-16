0b1d4ed79146bc5df5b63a53b02257a656d44239
Pooja Todkar <poojavk21@gmail.com>
2019-08-16 12:40:35 +0530

    added '/' get function

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require("path");
 const cors = require('cors');
 const express_graphql = require('express-graphql');
 const { buildSchema } = require('graphql');
