1bb55079e18f20d62ed4d01b76f950f7029ad1c0
Pooja Todkar <poojavk21@gmail.com>
2019-08-16 11:24:42 +0530

    added port

--- server.js
+++ server.js
@@ -701,4 +701,4 @@ app.use('/graphql', cors(), express_graphql({
     rootValue: root,
     graphiql: true
 }));
-app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
+app.listen(process.env.PORT || 4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
